ef34d5f7ae09ec652cdd66e943437b43b5073b98
PonamarenkoSS <116379293+PonamarenkoSS@users.noreply.github.com>
2024-03-24 13:28:23 +0400

    Create dag_1.py

---
+++ dags/dag_1.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import BranchPythonOperator, PythonOperator
+from airflow.operators.http_operator import SimpleHttpOperator
+import json
+from airflow.models import Variable
+
+API_key = Variable.get('openweather_key')
+url = f"/data/2.5/weather?lat=53.12&lon=50.06&appid={API_key}&units=metric"
+
+def get_weather(ti):
+    current_temp = ti.xcom_pull(task_ids='get_weather')
+    if current_temp > 15:
+        return 'warm_branch'
+    return 'cold_branch'
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 3, 23),
+    'retries': 1,
+}
+
+dag = DAG(
+    'OpenWeatherApi',
+    default_args=default_args,
+    description='DAG to fetch and display temperature from OpenWeather API',
+    schedule_interval=None,
+)
+
+get_response = SimpleHttpOperator(
+    task_id='get_weather',
+    method='GET',
+    http_conn_id='openweather',
+    endpoint=url,
+    response_filter=lambda response: response.json()["main"]["temp"],
+    headers={},
+    dag=dag
+)
+
+check_weather_branch = BranchPythonOperator(
+    task_id='check_branch',
+    python_callable=get_weather,
+    dag=dag,
+)
+
+warm_branch = PythonOperator(
+    task_id='warm_branch',
+    python_callable=lambda ti: print(f'ТЕПЛО: {ti.xcom_pull(task_ids="check_branch")}°C'),
+    dag=dag
+)
+
+cold_branch = PythonOperator(
+    task_id='cold_branch',
+    python_callable=lambda ti: print(f'ХОЛОДНО: {ti.xcom_pull(task_ids="check_branch")}°C'),
+    dag=dag
+)
+
+get_response >> check_weather_branch
+check_weather_branch >> warm_branch
+check_weather_branch >> cold_branch
+
